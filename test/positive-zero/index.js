3fa41a4a60df49d60b49bff9bb2aa628189f31ac
byr-gdp <anthozoan77@gmail.com>
2016-03-25 12:36:11 +0800

    add eg: positive-zero

---
+++ test/positive-zero/index.js
@@ -0,0 +1,6 @@
+var positiveZero = require('positive-zero');
+
+console.log('+0: ' + positiveZero(+0));
+console.log('-0: ' + positiveZero(-0));
+console.log('+1: ' + positiveZero(+1));
+console.log('-1: ' + positiveZero(-1));
